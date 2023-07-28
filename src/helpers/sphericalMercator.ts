89b0d49d6f62128882099e7eb2ea5e4240d1372e
Pieter van Mill <pietervanmill@hotmail.com>
2023-07-28 20:29:50 +0200

    Bit more efficient mercator fn.

--- src/helpers/sphericalMercator.ts
+++ src/helpers/sphericalMercator.ts
@@ -6,14 +6,11 @@ export function latlonToWorldCoordinates(latlon: LatLon): Point {
     const D = Math.PI / 180;
     const MAX_LATITUDE = 85.0511287798;
 
-    let latitude = latlon.latitude;
-    let longitude = latlon.longitude;
-
     // Clamp latitude to the range [-85.0511, 85.0511]
-    latitude = Math.max(Math.min(latitude, MAX_LATITUDE), -MAX_LATITUDE);
-    const sin = Math.sin(latitude * D);
+    const latitude = Math.max(Math.min(latlon.latitude, MAX_LATITUDE), -MAX_LATITUDE);
 
-    const worldX = R * longitude * D;
+    const sin = Math.sin(latitude * D);
+    const worldX = R * latlon.longitude * D;
     const worldY = R * Math.log((1 + sin) / (1 - sin)) / 2;
 
     // Return the pixel coordinates as TXY
